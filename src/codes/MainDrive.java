97dc75d1052eef3e83f00281c1578fae382ee192
조경진 <cho881020@gmail.com>
2021-07-09 20:08:48 +0900

    검사 결과에 따른 배열 처리 / 안내문구 처리

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -24,7 +24,7 @@ public class MainDrive {
 //				for문 보다도 앞에 Scanner를 만들어두고, 여기서는 활용만 하자.
 				
 //				안내문구 : ?번째 숫자 입력
-				System.out.print((i+1)+"번째 숫자 입력");
+				System.out.print((i+1)+"번째 숫자 입력 : ");
 				
 //				일단 입력받은 숫자를 변수에 담아두고 => 변수가 검사를 통과하면 배열에 넣자.
 				
@@ -52,6 +52,23 @@ public class MainDrive {
 						break;
 					}
 					
+				}
+				
+				
+//				두개의 검사를 모두 통과했다면, => 배열에 입력값을 대입 + 다음숫자 받으러 이동.
+				
+				if (isRangeOk && isDuplicateOk) {
+//					배열의 i번째 자리에 입력값 대입.
+					myLottoNumbers[i] = inputNum;
+					
+//					다음숫자 입력받으러 이동. => 무한반복을 깨주자. (for문이 다음바퀴로 이동)
+					break;
+				}
+				else if (!isRangeOk) {
+					System.out.println("1~45의 숫자만 입력가능합니다.");
+				}
+				else {
+					System.out.println("이미 입력한 숫자입니다.");
 				}
 				
 				
